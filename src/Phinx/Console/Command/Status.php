a082b26a1677761431c3156d1eee2e423c3b9f9e
Jamie <js@leadbyte.co.uk>
2024-04-29 16:03:18 +0100

    Added host option

--- src/Phinx/Console/Command/Status.php
+++ src/Phinx/Console/Command/Status.php
@@ -46,6 +46,7 @@ class Status extends AbstractCommand
         $this->setName('status')
             ->setDescription('Show migration status')
             ->addOption('--format', '-f', InputOption::VALUE_REQUIRED, 'The output format: text or json. Defaults to text.')
+            ->addOption('--host', '-o', InputOption::VALUE_REQUIRED, 'LeadByte host name')
             ->setHelp(
                 <<<EOT
 The <info>status</info> command prints a list of all migrations, along with their current status
